b2504cebd8f0435018bc6e3fc1b00a19e13c4c79
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-14 10:49:15 -0500

    create a pool.js file

---
+++ server/modules/pools/pool.js
@@ -0,0 +1,23 @@
+const Pool = require('pg');
+
+const config = {
+   database: 'time_tracker',
+   host: 'localhost',
+   port: 5432,
+   max: 10,
+   idleTimeOutMillis: 1000
+}
+
+const pool = new Pool(config);
+
+const DATABASE_NAME = 'time_tracker'
+
+pool.on('connect', ()=>{
+   console.log('Connected to database: ', DATABASE_NAME);
+});
+
+pool.on('error', (error)=>{
+   console.log('Error connecting to database: ', DATABASE_NAME, error)
+});
+
+module.exports = pool;
