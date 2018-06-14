6c2ca2766ce0c2eec2d1f85baa799d3edd181795
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-14 13:30:47 -0500

    add get queries to database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
--- Table creation and dummy data for testing
+-- TABLES
 create table entry (
 	id serial primary key,
 	"description" varchar(80),
@@ -12,7 +12,7 @@ create table project (
 	"name" varchar(100)
 );
 
-
+-- Dummy Data
 insert into project ("name")
 values
 ('Time-tracker'),
@@ -28,3 +28,13 @@ values
 ('Read up on REACT framework to decide if app should be built with REACT', '5/31/2018', 1, 2),
 ('World-building', '5/24/2018', 2, 3),
 ('Plotted narrative', '5/26/2018', 3, 3);
+
+-- Query for data to display on history view page
+select entry.id as entry_id, entry.description as description, entry.date as date, entry.hours as hours, project_id, project.name as project from entry 
+join project on entry.project_id = project.id
+order by entry.date desc;
+
+-- Query for data to display on project view page
+select project.id, project.name as project, sum(entry.hours) as hours from project
+join entry on project.id = entry.project_id
+group by project.id;
