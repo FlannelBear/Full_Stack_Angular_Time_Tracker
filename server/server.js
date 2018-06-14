c53b0fa9a0a51f8b65e85d3b15ba21c4785dac37
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-14 10:56:39 -0500

    revert server.js to pre-testing state

--- server/server.js
+++ server/server.js
@@ -7,21 +7,6 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
-const pool = require('./modules/pools/pool');
-
-app.get('/test', (req, res)=>{
-   const queryText = 'SELECT * FROM dummyData;';
-   pool.query(queryText).then((result)=>{
-      console.log('Got it!');
-      res.send(result.rows);
-   }).catch((error)=>{
-      console.log('Error handling GET for /test', error);
-      res.sendStatus(404);
-   });
-});
-
-
-
 app.listen(PORT, ()=>{
    console.log('Server running on port: ', PORT);
 });
