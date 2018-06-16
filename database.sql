7728c757d147cff61bd8fe52dcd9eea11495bae9
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-16 16:23:01 -0500

    update database.sql with new queries

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
--- TABLES
+-- Tables
 create table entry (
 	id serial primary key,
 	"description" varchar(80),
 	"date" date,
-	"hours" int,
-	"project_id" int references project
+	"hours" decimal,
+	"project_id" int references project on delete cascade
 );
 
 create table project (
@@ -12,7 +12,7 @@ create table project (
 	"name" varchar(100)
 );
 
--- Dummy Data
+-- Dummy Data for testing
 insert into project ("name")
 values
 ('Time-tracker'),
@@ -28,13 +28,3 @@ values
 ('Read up on REACT framework to decide if app should be built with REACT', '5/31/2018', 1, 2),
 ('World-building', '5/24/2018', 2, 3),
 ('Plotted narrative', '5/26/2018', 3, 3);
-
--- Query for data to display on history view page
-select entry.id as entry_id, entry.description as description, entry.date as date, entry.hours as hours, project_id, project.name as project from entry 
-join project on entry.project_id = project.id
-order by entry.date desc;
-
--- Query for data to display on project view page
-select project.id, project.name as project, sum(entry.hours) as hours from project
-join entry on project.id = entry.project_id
-group by project.id;
