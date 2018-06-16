2004c17f8facca547bfabe6c14a97cfb13b4069c
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-16 17:04:02 -0500

    add sort functionality to entry table

--- server/public/scripts/controllers/entry.controller.js
+++ server/public/scripts/controllers/entry.controller.js
@@ -28,7 +28,57 @@ app.controller('EntryController', ['TrackerService', function(TrackerService){
       });
    }
 
-   
+   let clicked = false;
+   vm.sortEntries = function(click){
+      clicked = !clicked;
+      
+      clicked == true ? sortA(click) : sortB(click);
+   }
+
+   sortB = function(click){
+      if(click == 'description'){
+         vm.entryList = vm.entryList.sort(function(a, b) {
+            const B = b.description.toLowerCase();
+            const A = a.description.toLowerCase();
+            return B < A ? -1 : B > A ? 1 : 0;
+          });
+          console.log(vm.entryList);
+      } else if(click == 'project'){
+         vm.entryList = vm.entryList.sort(function(a, b) {
+            return b.project_id - a.project_id;
+          });
+      } else if(click == 'date'){
+         vm.entryList = vm.entryList.sort(function(a, b) {
+            return Date.parse(b.date) - Date.parse(a.date);
+          });
+      } else if(click == 'hours'){
+         vm.entryList = vm.entryList.sort(function(a, b) {
+            return b.hours - a.hours;
+          });
+      }
+   }
+
+   sortA = function(click){
+      if(click == 'description'){
+         vm.entryList = vm.entryList.sort(function(a, b) {
+            const A = a.description.toLowerCase();
+            const B = b.description.toLowerCase();
+            return A < B ? -1 : A > B ? 1 : 0;           
+          });
+      } else if(click == 'project'){
+         vm.entryList = vm.entryList.sort(function(a, b) {
+            return a.project_id - b.project_id;
+          });
+      } else if(click == 'date'){
+         vm.entryList = vm.entryList.sort(function(a, b) {
+            return Date.parse(a.date) - Date.parse(b.date);
+          });
+      } else if(click == 'hours'){
+         vm.entryList = vm.entryList.sort(function(a, b) {
+            return a.hours - b.hours;
+          });
+      }
+   }
    
    vm.getEntries();
    vm.getProjects();
