325e282e77750d13ab4f90861cf899059a5aa564
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-16 19:38:32 -0500

    add responsive bar-graph that updates as entries are made and projects are added

--- server/modules/routes/report.router.js
+++ server/modules/routes/report.router.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../pools/pool');
 
-
-
+router.get('/:need', (req, res)=>{
+   if(req.params.need === 'label'){
+      queryText = 'select project.name from project;';
+      pool.query(queryText).then((result)=>{
+         console.log(result.rows);
+         res.send(result.rows);
+      }).catch((error)=>{
+         console.log('Err', error);
+         res.sendStatus(404);
+      });
+   } else if(req.params.need === 'data'){
+      queryText = `select sum(entry.hours) as hours from project
+      join entry on project.id = entry.project_id
+      group by project.id;`;
+      pool.query(queryText).then((result)=>{
+         console.log(result.rows);
+         res.send(result.rows);
+      }).catch((error)=>{
+         console.log('Err', error);
+         res.sendStatus(404);
+      });
+   }
+});
 
 
 module.exports = router;
--- server/public/scripts/controllers/report.controller.js
+++ server/public/scripts/controllers/report.controller.js
@@ -1,46 +1,63 @@
 app.controller('ReportController', ['TrackerService', function(TrackerService){
    const vm = this;
    console.log('In Report');
-   const ctx = document.getElementById("myChart");
 
+   let chartLabels = [];
+   let chartData = [];
 
-   
+   vm.getLabels = function(){
+      chartLabels = [];
+      TrackerService.get('report/label').then(function(){
+            for(let object of TrackerService.data){
+               chartLabels.push(object.name);
+            }
+         });
+         vm.getData();
+         console.log(chartLabels);
+   }
 
+   vm.getData = function(){
+      chartData = [];
+      TrackerService.get('report/data').then(function(){
+         for(let object of TrackerService.data){
+            chartData.push(object.hours);
+         }
+         vm.getChart();
+         });
+         console.log(chartData);
+   }
 
-   const myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: vm.chartLabels,
-          datasets: [{
-              label: '# of Hours',
-              data: vm.chartData,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
+   vm.getChart = function(){
+      let barChart = document.getElementById('barChart').getContext('2d');
+      console.log(chartLabels);
+      console.log(chartData);
+      let timeChart = new Chart(barChart, {
+         type: 'bar',
+         data: {
+            labels: chartLabels,
+            datasets: [
+               {
+                  label: 'Hours Per Project',
+                  backgroundColor: "rgba(50,200,200,0.2)",
+                  borderColor: "rgba(50,200,200,1)",
+                  borderWidth: 2,
+                  hoverBackgroundColor: "rgba(50,50,50,0.4)",
+                  data: chartData
+               }
+            ]
+         },
+         options: {
+            scales: {
+               yAxes: [{
                   ticks: {
-                      beginAtZero:true
+                     beginAtZero: true
                   }
-              }]
-          }
-      }
-  }); // end chart config
+               }]
+            }
+         }
+      });
+   }
+
+   vm.getLabels();
+
 }]);
